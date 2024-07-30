3c58b32570f2a34f49dc7c6eab6c1ad99d7b44f9
alejandro <luismencos571@gmail.com>
2024-07-30 07:24:55 -0600

    arreglo

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,19 +4,19 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarPrincipal">
-             <img src="../src/images/logo GYM.jpg" class="d-block" width="140px">
+             <img src="/mencos_GYM/src/images/logo GYM.jpg" class="d-block" width="140px">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         SERVICIOS
                     </a>
                     <ul class="dropdown-menu bg-light">
                         <li class="nav-item">
-                            <a class="nav-link" href="mencos_GYM/views/miembros/index.php">MIEMBROS</a>
-                            <a class="nav-link" href="mencos_GYM/views/instructores/index.php">INSTRUCTORES</a>
+                            <a class="nav-link" href="/mencos_GYM/views/miembros/index.php">MIEMBROS</a>
+                            <a class="nav-link" href="/mencos_GYM/views/instructores/index.php">INSTRUCTORES</a>
                             <a class="nav-link" href="#">INSCRIPCIONES</a>
                         </li>
                     </ul>
